14797c37eebe1ac08ef7d1336748b1d4ab4824dd
gnanesh007.mk@gmail.com <gnanesh007.mk@gmail.com>
2019-12-30 13:41:52 +0530

    first commit

---
+++ day1.rb
@@ -0,0 +1,20 @@
+puts"hello there"
+def sayhello
+   puts "hey"
+end
+
+sayhello
+
+def sayhello(name)
+   puts "hey"+name
+end
+
+sayhello("kumar")
+sayhello "ram"
+
+def sayhello(name="no name")
+   puts "hey"+name
+end
+
+sayhello("kumar")
+sayhello
